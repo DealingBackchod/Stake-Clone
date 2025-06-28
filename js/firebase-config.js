d2c347eea40d38de70c52afc109214639a59f8e8
DealingBackchod <dealing949@gmail.com>
2025-06-28 23:07:58 +0530

    firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,10 @@
+// Replace with your Firebase config
+const firebaseConfig = {
+  apiKey: "YOUR-API-KEY",
+  authDomain: "YOUR-PROJECT.firebaseapp.com",
+  projectId: "YOUR-ID",
+  storageBucket: "YOUR.appspot.com",
+  messagingSenderId: "SENDER-ID",
+  appId: "APP-ID"
+};
+firebase.initializeApp(firebaseConfig);
